138d6e67c118aca518420864199ceb615379b0da
FlorenciaCampos <florenciacampos460@gmail.com>
2025-06-16 14:23:17 -0300

    se agregan pages Nosotros, Contacto

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Contacto = () => {
+    return (
+      <header className='header'>
+        <h1>Contacto</h1>
+      </header>
+    );
+  };
+
+export default Contacto;
---
+++ src/pages/Nosotros.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Nosotros = () => {
+    return (
+      <header className='header'>
+        <h1>Nosotros</h1>
+      </header>
+    );
+  };
+
+export default Nosotros;
